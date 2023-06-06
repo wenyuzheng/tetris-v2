d2dc2cc1fa4ca4a122f84d0a5024cb93684bf1b2
Wenyu Zheng <zhengwy229@gmail.com>
2023-06-06 22:42:45 +0100

    Fix runPiece

--- src/lib/classes/Game.js
+++ src/lib/classes/Game.js
@@ -36,13 +36,11 @@ class Game {
           this.score++;
 
           this.piece = generatePiece(this.board.width, this.board.height);
-          // console.log(this.piece);
+          console.log(this.piece);
 
           if (!timeInterval <= 100 && this.score % 2 === 0) {
             timeInterval -= 100;
           }
-
-          this.runPiece(timeInterval);
         } else {
           console.log("end game");
           clearInterval(setInt);
@@ -54,72 +52,6 @@ class Game {
       }
     }, timeInterval);
   }
-
-  // runPiece(timeInt = 1000) {
-  //   let timeInterval = timeInt;
-
-  //   const setInt = setInterval(() => {
-  //     this.movePiece("down");
-  //     console.log("aftermove", this.piece.cells);
-
-  //     if (isPieceAtBottom(this.piece, this.board)) {
-  //       this.board.addCells(this.piece.cells);
-  //       this.board.removeFullRows();
-
-  //       this.score++;
-
-  //       this.piece = generatePiece(this.board.width, this.board.height);
-  //       // console.log(this.piece);
-
-  //       if (!timeInterval <= 100 && this.score % 2 === 0) {
-  //         timeInterval -= 100;
-  //       }
-
-  //       this.runPiece(timeInterval);
-  //       if (this.isEndGame()) {
-  //         console.log("end game");
-  //         console.log("board cells", this.board.cellsArr);
-  //         clearInterval(setInt);
-  //       }
-  //     }
-  //   }, timeInterval);
-  // }
-
-  // runPiece(timeInterval = 1000) {
-  //   const setInt = setInterval(() => {
-  //     const result = this.isEndGame();
-  //     console.log({ result });
-  //     this.movePiece("down");
-  //     console.log("aftermove", this.piece.cells);
-
-  //     if (isPieceAtBottom(this.piece, this.board)) {
-  //       this.board.addCells(this.piece.cells);
-  //       this.board.removeFullRows();
-  //       console.log("board cells", this.board.cellsArr);
-  //     }
-  //   }, timeInterval);
-
-  //   return setInt;
-  // }
-
-  // // run(timeInterval = 1000) {
-  // //   this.runPiece(timeInterval);
-  // // }
-
-  // run(timeInterval = 1000) {
-  //   let intervalId = this.runPiece();
-
-  //   // Clear the interval after a certain condition is met
-  //   const result = this.isEndGame();
-
-  //   if (result) {
-  //     clearInterval(intervalId);
-  //     console.log("Interval block cleared");
-
-  //     // Code block to run after the interval block is cleared
-  //     console.log("Running code block after clearInterval");
-  //   }
-  // }
 }
 
 export default Game;
